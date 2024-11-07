02b17809fe3757374751512a728e2b08c56937d4
viniciussouza429 <vinicius.souza429@hotmail.com>
2024-11-07 00:06:18 -0300

    add git hooks

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,7 +24,7 @@ const LoginPage = async () => {
         <p className="text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+          facilitando o controle do seu orçamento
         </p>
         <SignInButton>
           <Button variant="outline">
